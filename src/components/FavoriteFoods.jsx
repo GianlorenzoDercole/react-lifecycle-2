7b6a3f7c156df8c98519a5477f5057d2e2daf01c
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-21 16:25:23 -0700

    basic setup

---
+++ src/components/FavoriteFoods.jsx
@@ -0,0 +1,13 @@
+
+import React , { Component } from 'react'
+
+export default class FavoriteFoods extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Favorite Foods</h1>
+        </div>
+
+    )
+  }
+}
